ad00f3bdabeebbe2d126f880e7f202411b864ac9
Xiao Fu <xiao.fu1790@gmail.com>
2020-03-09 12:56:35 -0400

    generated

--- app/src/androidTest/java/com/example/testapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/testapp/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.testapp;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
